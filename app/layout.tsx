ec81f65851b75c7a47a5d9c5d574910a1d446845
fardeenpathan <md.fardeen5236@gmail.com>
2026-06-01 11:35:03 +0530

    update og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,12 +14,12 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://hitched-rohanclientdemo.vercel.app/"),
+  metadataBase: new URL("https://hitched-devnawedsrohan.vercel.app/"),
 
   openGraph: {
     title: "Devna & Rohan",
     description: "We invite you to celebrate our forever",
-    url: "https://hitched-rohanclientdemo.vercel.app/",
+    url: "https://hitched-devnawedsrohan.vercel.app/",
     siteName: "InviteArc",
     images: [
       {
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
   },
 
  other: {
-    "og:image:secure_url": "https://hitched-rohanclientdemo.vercel.app/og.jpg",
+    "og:image:secure_url": "https://hitched-devnawedsrohan.vercel.app/og.jpg",
     "og:image:type": "image/jpeg",
   },
 
